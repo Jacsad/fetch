77f8e58cfa1d64fa600d30161ea970fe4e9ff13a
Jacsad <yakov94@gmail.com>
2022-05-03 12:07:59 +0300

    Fetch Homework

---
+++ app.js
@@ -0,0 +1,12 @@
+fetch("http://localhost:3001/profile")
+.then(userInfo=>userInfo.json())
+.then(userInfoJSON=>display(userInfoJSON));
+
+function display(userData){
+    document.getElementById("printUserName").innerHTML = userData.username;
+    document.getElementById("printFullName").innerHTML = userData.fullname;
+    document.getElementById("printLanguages").innerHTML = userData.languages;
+    document.getElementById("printPermission").innerHTML = userData.permission;
+}
+
+
